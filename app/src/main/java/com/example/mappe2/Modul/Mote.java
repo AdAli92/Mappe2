6db3f50dbe149db06bf233a352501dc71ae4c9d7
Adnan <adnag>
2020-10-17 16:41:19 +0200

    fff

--- app/src/main/java/com/example/mappe2/Modul/Mote.java
+++ app/src/main/java/com/example/mappe2/Modul/Mote.java
@@ -3,7 +3,7 @@ package com.example.mappe2.Modul;
 public class Mote {
 
     private int moteId;
-    private String navn, type, dato, sted;
+    private String navn, type, dato, sted, tid;
     private int img;
 
     public int getMoteId() {
@@ -14,29 +14,31 @@ public class Mote {
         this.moteId = moteId;
     }
 
-    public Mote(String navn, String type, String dato, String sted, int img) {
+    public Mote(String navn, String type, String dato, String sted, String tid, int img) {
         this.navn = navn;
         this.type = type;
         this.dato = dato;
         this.sted = sted;
         this.img = img;
+        this.tid = tid;
     }
 
-    public Mote(String navn, String type, String dato, String sted) {
+    public Mote(String navn, String type, String dato, String sted, String tid) {
         this.navn = navn;
         this.type = type;
         this.dato = dato;
         this.sted = sted;
+        this.tid = tid;
 
     }
 
-    public Mote(int moteId, String navn, String type, String dato, String sted) {
+    public Mote(int moteId, String navn, String type, String dato, String sted, String tid) {
         this.moteId = moteId;
         this.navn = navn;
         this.type = type;
         this.dato = dato;
         this.sted = sted;
-        this.img = img;
+        this.tid = tid;
     }
 
     public Mote(){
@@ -73,6 +75,14 @@ public class Mote {
 
     public void setSted(String sted) {
         this.sted = sted;
+    }
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
     }
 
     public int getImg() {
